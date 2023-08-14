16651e505bb73a0db4aa6c317b6a876adc1bd1eb
Freek-ZIGZAG <112490298+Freek-ZIGZAG@users.noreply.github.com>
2023-08-15 00:45:17 +0200

    changes

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,19 @@
 
+import { browser } from "$app/environment";
+import { onMount } from "svelte";
 import "../app.scss"
+import { StatusBar, Style } from '@capacitor/status-bar';
+import { Capacitor } from "@capacitor/core";
 
 export const prerender = true;
 
+(async () => {
+    if(!browser) return;
+    const platform = Capacitor.getPlatform();
+    const allowed= ["android", "ios"];
+    console.log(platform);
+    if(!allowed.includes(platform)) return;
+    await StatusBar.setStyle({ style: Style.Dark })
+    await StatusBar.hide();
+    
+})();
