7b0a66601f295a96af01eae53909e9aae36e03c6
Freekiehsoes <freekattema@live.nl>
2023-08-17 15:21:07 +0200

    changes

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,6 +4,8 @@ import { onMount } from "svelte";
 import "../app.scss"
 import { StatusBar, Style } from '@capacitor/status-bar';
 import { Capacitor } from "@capacitor/core";
+import { App as CapacitorApp } from '@capacitor/app';
+import { ScreenOrientation } from '@capacitor/screen-orientation';
 
 export const prerender = true;
 
@@ -11,16 +13,28 @@ export const prerender = true;
     if (!browser) return;
     const platform = Capacitor.getPlatform();
     const allowed = ["android", "ios"];
-    console.log(platform);
     if (!allowed.includes(platform)) return;
+    await fixes();
 
-    const is_android = platform === "android";
+    setInterval(async () => {
+        await fixes();
+    }, 5000);
+})();
 
-    if (is_android) {
-        await StatusBar.setOverlaysWebView({ overlay: true });
-    }
+async function fixes() {
 
+    await StatusBar.setOverlaysWebView({ overlay: true });
     await StatusBar.setStyle({ style: Style.Dark })
     await StatusBar.hide();
+    await ScreenOrientation.lock({
+        orientation: 'portrait'
+    })
 
-})();
+    CapacitorApp.addListener('backButton', ({ canGoBack }) => {
+        if (!canGoBack) {
+            CapacitorApp.exitApp();
+        } else {
+            history.back();
+        }
+    });
+}
